aa85ed59cd05fa580e068b6d2e1da3049619ba09
Ze-Marcos <gringojaimes20@gmail.com>
2019-02-23 23:29:24 -0800

    adding a Header and Footer components, will be eventually Pure Component beacuse Header and footer will be static

---
+++ todo-app/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css-scss/Header.css";
+
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                Footer time 
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ todo-app/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css-scss/Header.css";
+
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                Header Time!
+            </div>
+        );
+    }
+}
+
+export default Header;
