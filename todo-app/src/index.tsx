2d7df45fd4f95b6b7c9f349001ba28173353b55f
Ze-Marcos <gringojaimes20@gmail.com>
2019-02-25 12:17:14 -0800

    added ability to use bootstrap and use the jquery with bootstrap

--- todo-app/src/index.tsx
+++ todo-app/src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "jquery/dist/jquery.js";
+import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.css";
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter, Route, Link } from "react-router-dom";
